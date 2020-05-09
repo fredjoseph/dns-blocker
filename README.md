4b5fc4b2ef690de56127dbe06d3634f422c14231
Frederic JOSEPH <fredjoseph@users.noreply.github.com>
2020-05-09 14:18:10 +0200

    Refactoring

--- README.md
+++ README.md
@@ -8,14 +8,14 @@ DNS Blocker for Chrome and Firefox (Web Extension)
 1. Visit `chrome://extensions/`
 2. Enable Developer mode.
 3. Click on `Load unpacked extension`
-4. Select the cloned folder.
+4. Select the `web-extension` directory of the cloned folder.
 (You'll get an "Disable dev mode extensions" popup, if you try restarting Chrome at this stage)
-5. Click on `Pack extension` and enter the root directory of the cloned folder (keep `pem file` empty)
+5. Click on `Pack extension` and enter the `web-extension` directory of the cloned folder (keep `pem file` empty)
 6. Uninstall the previously installed extension (as unpacked extension)
 7. Install the extension using the `.crx` file generated at step `5` and note its `ID`
 8. Add the following registry key 
     - Under `HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome\ExtensionInstallWhitelist` (create it if not exists), create a new string for each extension you want to enable with sequential names (indices), e.g. 1, 2,...
-    - Enter the extension ID as string value. For example, there is a string with name `1` and value `nmgnihglilniboicepgjclfiageofdfj`
+    - Enter the extension ID as string value. For example: there is a string with name `1` and value `nmgnihglilniboicepgjclfiageofdfj`
 9. Restart Chrome
 10. You can check that the policy is working in Chrome by opening `chrome://policy`
 
@@ -25,7 +25,7 @@ In order to override the extension shortcuts : go to `chrome://extensions/shortc
 1. Clone this repository
 2. Visit `about:debugging`
 3. click `This Firefox`
-4. click `Load Temporary Add-on` then select the any file inside the extension's directory
+4. click `Load Temporary Add-on` then select the any file inside the `web-extension` directory
 (At this stage, the extension will be removed if you restart Firefox - The extension must be signed to be installed permanently)
 5. Generate a `zip` file of the extension
 6. Submit the archive on `https://addons.mozilla.org/developers/` (requires a Firefox account)
--- scripts/updater.py
+++ scripts/updater.py
@@ -1,7 +1,6 @@
-import requests
-import datetime
-import json
+import sys, argparse, requests, datetime, json
 import subprocess as cmd
+from pathlib import Path
 
 STEVEN_URL = "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts"
 BLOCKER_DNS_URL = "https://blockerdns.com/hosts-ads.json"
@@ -11,35 +10,45 @@ def diff(first, second):
     second = set(second)
     return [item for item in first if item not in second]
 
-with requests.get(STEVEN_URL, stream=True) as r1:
-    steven_list = ["*://{}/*".format(line.split(' ')[1]) for line in r1.iter_lines(decode_unicode=True) if line.startswith('0.0.0.0')]
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--publish", help="push updates directly to 'origin master'", action="store_true")
+    args = parser.parse_args()
 
-blocker_dns_list = requests.get(BLOCKER_DNS_URL).json()['adDomains']
-blocker_dns_list_filtered = diff(blocker_dns_list, steven_list)
+    with requests.get(STEVEN_URL, stream=True) as r1:
+        steven_list = ["*://{}/*".format(line.split(' ')[1]) for line in r1.iter_lines(decode_unicode=True) if line.startswith('0.0.0.0')]
 
-my_list = requests.get(MY_DNS_URL).json()['domains']
-my_list_filtered = diff(my_list, steven_list)
+    blocker_dns_list = requests.get(BLOCKER_DNS_URL).json()['adDomains']
+    blocker_dns_list_filtered = diff(blocker_dns_list, steven_list)
 
-full_list = steven_list + blocker_dns_list_filtered + my_list_filtered
+    my_list = requests.get(MY_DNS_URL).json()['domains']
+    my_list_filtered = diff(my_list, steven_list)
 
-data = {
-    'version': datetime.datetime.now().date().isoformat(),
-    'domains': full_list
-}
+    full_list = steven_list + blocker_dns_list_filtered + my_list_filtered
 
-print('Steven List: {0} elements'.format(len(steven_list)))
-print('Blocker DNS List: {0} elements (unique: {1})'.format(len(blocker_dns_list), len(blocker_dns_list_filtered)))
-print('My DNS List: {0} elements (unique: {1})'.format(len(my_list), len(my_list_filtered)))
-print('Total Elements : {0}'.format(len(full_list)))
+    data = {
+        'version': datetime.datetime.now().date().isoformat(),
+        'domains': full_list
+    }
 
-with open('../data.json', 'r') as current_file:
-    current_data = json.load(current_file)
+    print('Steven List: {0} elements'.format(len(steven_list)))
+    print('Blocker DNS List: {0} elements (unique: {1})'.format(len(blocker_dns_list), len(blocker_dns_list_filtered)))
+    print('My DNS List: {0} elements (unique: {1})'.format(len(my_list), len(my_list_filtered)))
+    print('Total Elements : {0}'.format(len(full_list)))
 
-if len(current_data['domains']) != len(data['domains']) or len(diff(data['domains'], current_data['domains'])):
-    with open('../data.json', 'w') as out_file:
-        json.dump(data, out_file, indent=4)    
-    cmd.run("git add .", check=True, shell=True)
-    cmd.run("git commit -m \"Update list of blocked domains\"", check=True, shell=True)
-    cmd.run("git push", check=True, shell=True)
-else:
-    print('No change to commit')
+    data_path = Path(__file__).parent / '../web-extension/data.json'
+    with open(data_path, 'r') as current_file:
+        current_data = json.load(current_file)
+
+    if len(current_data['domains']) != len(data['domains']) or len(diff(data['domains'], current_data['domains'])):
+        with open(data_path, 'w') as out_file:
+            json.dump(data, out_file, indent=4)
+        if args.publish:
+            cmd.run("git add .", check=True, shell=True)
+            cmd.run("git commit -m \"Update list of blocked domains\"", check=True, shell=True)
+            cmd.run("git push", check=True, shell=True)
+    else:
+        print('No change to commit')
+   
+if __name__ == "__main__":
+   main(sys.argv[1:])
